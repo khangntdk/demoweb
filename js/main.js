34d5ec2d9afebd52304fcb0bbf4771dfc830e116
dangkhoaabc <dangkhoaabc19@gmail.com>
2026-07-04 14:04:01 +0700

    update

--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-  const menuBtn = document.getElementById("menuBtn");
-  const mobileNav = document.getElementById("mobileNav");
-
-  menuBtn.addEventListener("click", () => {
-    mobileNav.classList.toggle("open");
-  });
